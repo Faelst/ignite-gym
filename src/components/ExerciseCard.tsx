2f2082020f6e3c3680258a37207426d14e47953c
Rafael Silverio <fael_st@hotmail.com>
2022-12-01 01:32:45 -0300

    flat list of exercises

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,43 @@
+import { Heading, HStack, Icon, Image, Text, VStack } from 'native-base'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+type Props = TouchableOpacityProps & {}
+
+export function ExerciseCard({ ...rest }: Props) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack bg="gray.500" alignItems="center" p={2} rounded="md" mb={5}>
+        <Image
+          source={{
+            uri: 'https://www.wikihow.com/images_en/thumb/d/db/Work-Your-Back-With-Dumbbells-Step-2-Version-5.jpg/v4-460px-Work-Your-Back-With-Dumbbells-Step-2-Version-5.jpg',
+          }}
+          alt="Exercício de costas"
+          w={16}
+          h={16}
+          rounded="md"
+          mr={4}
+          resizeMode="center"
+        />
+
+        <VStack flex={1} ml={4} justifyContent="center">
+          <Heading color="gray.100" fontSize="md">
+            Remada curvada
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm" numberOfLines={2}>
+            3 séries x 10 repetições
+          </Text>
+        </VStack>
+
+        <Icon
+          as={Entypo}
+          name="chevron-thin-right"
+          color="gray.300"
+          size={4}
+          pr={5}
+        />
+      </HStack>
+    </TouchableOpacity>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,72 @@
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { HStack, VStack } from 'native-base'
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base'
 import { useState } from 'react'
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('')
+  const [groups, setGroups] = useState(['costa', 'ombro', 'peito', 'perna'])
+  const [exercises, setExercises] = useState([
+    {
+      name: 'Supino Reto',
+      group: 'peito',
+      description: 'Supino reto com barra',
+      series: 3,
+      repetitions: 10,
+    },
+    {
+      name: 'Supino Reto',
+      group: 'peito',
+      description: 'Supino reto com barra',
+      series: 3,
+      repetitions: 10,
+    },
+  ])
 
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 5,
+        }}
+        my={6}
+        maxH={10}
+      />
 
-      <HStack mt={6} px={2}>
-        <Group
-          name="costas"
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')}
+      <VStack flex={1} px={4}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20,
+          }}
         />
-        <Group
-          name="peito"
-          isActive={groupSelected === 'peito'}
-          onPress={() => setGroupSelected('peito')}
-        />
-      </HStack>
+      </VStack>
     </VStack>
   )
 }
